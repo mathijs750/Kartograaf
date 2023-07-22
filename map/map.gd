e75e592c5be2796037880dfd7979bfb48a60f11d
Mathijs Lemmers <mathijs750@gmail.com>
2023-07-23 00:33:40 +0200

    Add tile check logic

--- map/map.gd
+++ map/map.gd
@@ -1,29 +1,18 @@
 extends Node2D
 
-signal tile_selected
-signal tile_placed
+signal tile_selected(tile_coords, isLand, isWater, isForrest, isLandmark)
+signal tile_placed(tile_coords, isLand, isWater, isForrest, isLandmark)
 
-var current_tile: TileData = null
+var current_tile_data: TileData = null
 var current_source_id: int = -1
 var current_atlas_coords: Vector2i = Vector2i(-1,-1)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
 func _input(event):
-	if not $".".is_visible():
-		pass
+	if not is_visible():
+		return
 	
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("Left button was clicked at ", event.position)
-			print($TileMap.local_to_map(event.position))
-
 			if event.position.x <= 1087:
 				print("Placing tile")
 				placeTile(event.position)
@@ -35,14 +24,23 @@ func _input(event):
 
 func getTileData(screen_pos):
 	var map_pos = $TileMap.local_to_map(screen_pos)
-	current_tile = $TileMap.get_cell_tile_data(0, map_pos)
+	current_tile_data = $TileMap.get_cell_tile_data(0, map_pos)
 	current_source_id = $TileMap.get_cell_source_id(0, map_pos)
 	current_atlas_coords = $TileMap.get_cell_atlas_coords(0, map_pos)
-
-	print(current_tile)
+	tile_selected.emit(map_pos, 
+			current_tile_data.get_custom_data_by_layer_id(0),
+			current_tile_data.get_custom_data_by_layer_id(1),
+			current_tile_data.get_custom_data_by_layer_id(2),
+			current_tile_data.get_custom_data_by_layer_id(3) )
 
 func placeTile(screen_pos):
-	if current_tile != null:
-		$TileMap.set_cell(0, $TileMap.local_to_map(screen_pos), current_source_id, current_atlas_coords)
+	if current_tile_data != null:
+		var map_pos = $TileMap.local_to_map(screen_pos)
+		$TileMap.set_cell(0, map_pos, current_source_id, current_atlas_coords)
+		tile_placed.emit(map_pos, 
+			current_tile_data.get_custom_data_by_layer_id(0),
+			current_tile_data.get_custom_data_by_layer_id(1),
+			current_tile_data.get_custom_data_by_layer_id(2),
+			current_tile_data.get_custom_data_by_layer_id(3) )
 	else:
 		print("No tile selected")
--- world/WorldMap.gd
+++ world/WorldMap.gd
@@ -1,11 +1,24 @@
 extends TileMap
 
+signal tile_sampled(tile_coords, isLand, isWater, isForrest, isLandmark)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var current_tile_data: TileData = null
+
+func _input(event):
+	if not is_visible():
+		return
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			sampleTile(event.position)
+
+func sampleTile(screen_pos):
+	var map_pos = $TileMap.local_to_map(screen_pos)
+	current_tile_data = $TileMap.get_cell_tile_data(0, map_pos)
+	tile_sampled.emit(map_pos, 
+			current_tile_data.get_custom_data_by_layer_id(0),
+			current_tile_data.get_custom_data_by_layer_id(1),
+			current_tile_data.get_custom_data_by_layer_id(2),
+			current_tile_data.get_custom_data_by_layer_id(3) )
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
